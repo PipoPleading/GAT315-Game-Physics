df2429b0cce704e22e83d463c678e778412296e1
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2024-04-08 11:56:13 -0600

    Character Controller Physics

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] public float playerSpeed = 2.0f;
+    [SerializeField] public float jumpHeight = 1.0f;
+    [SerializeField] public float rotationRate = 3.0f;
+    [SerializeField] Transform view;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float pushPower = 2.0f;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = Vector3.ClampMagnitude(move, 1);
+        // view space
+        move = Quaternion.Euler(0, view.rotation.eulerAngles.y, 0) * move;
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            //gameObject.transform.forward = move;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), Time.deltaTime * rotationRate);
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButton("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * Physics.gravity.y);
+        }
+
+        playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.AddForce(pushDir * pushPower, ForceMode.VelocityChange);
+        //body.velocity = pushDir * pushPower; //the same, but one is velocity rather than forcemode
+
+    }
+}
